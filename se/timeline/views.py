b0571f0fc9af3e1367b019270beaac31d6f840d9
Ubuntu <ubuntu@ip-172-31-83-198.ec2.internal>
2021-12-19 04:08:12 +0000

    datat 전처리

--- se/timeline/views.py
+++ se/timeline/views.py
@@ -51,7 +51,7 @@ class CourseRecommendAPI(APIView):
     '''
     def post(self, request):
         
-        data = request.data.dict()
+        data = request.data
 
         major = data['major']
         grade = data['grade']
@@ -61,4 +61,46 @@ class CourseRecommendAPI(APIView):
         major_list = data['major_list']
         ge_list = data['ge_list']
 
+        major_course = Course.objects.filter(
+            Q(major = major) & Q(grade = grade)
+        )
+
+        ge_course = Course.objects.filter(
+            Q(major = "서울 교양 전체")
+        )
+
+        major = {}
+        ge = {}
+
+        for m in major_course:
+            if "시간미지정강좌" in m.classTime: continue
+            tmp = make_time(m)
+            major[m.id] = {
+                'id': m.id,
+                'name': m.name,
+                'session': tmp,
+                'place': m.place
+            }
+
+        for g in ge_course:
+            if "시간미지정강좌" in g.classTime: continue
+            tmp = make_time(g)
+            ge[g.id] = {
+                'id': g.id,
+                'name': g.name,
+                'session': tmp,
+                'place': g.place
+            }
+            
         return Response(data)
+
+def make_time(string):
+    tmp = string.classTime.split(')')[:-1]
+    ret = [x.split('(') for x in tmp]
+
+    for r in ret:
+        r[1] = r[1].split("-")
+        r[1][0] = datetime.strptime(r[1][0], '%H:%M')
+        r[1][1] = datetime.strptime(r[1][1], '%H:%M')
+
+    return ret
